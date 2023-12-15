1bcfcad0d73bd20558ed723b095846f0c3cb2169
Mohamed Amarcha <simoamarcha@icloud.com>
2023-12-15 17:44:12 +0100

    memory qui marche ... toujours pas du tout

--- memory.c
+++ memory.c
@@ -25,20 +25,32 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 
 struct memory_data {
-    uint32_t address;
-    uint8_t *value;
+    uint8_t value8;
+    uint16_t value16;
+    uint32_t value32;
+    int fin;
 };
 
-static size_t t = 0;
-
 memory memory_create(size_t size) {
-    memory mem = NULL;
-    t = size;
+    memory mem = malloc(sizeof(struct memory_data) * (size + 1));
+    if (mem != NULL) {
+        for (size_t i = 0; i < size; i++) {
+            mem[i].value8 = NULL;
+            mem[i].value16 = NULL;
+            mem[i].value32 = NULL;
+            mem[i].fin = 0;
+        }
+        mem[size].fin = 1;
+    }
     return mem;
 }
 
 size_t memory_get_size(memory mem) {
-    return t;
+    size_t i = 0;
+    while (mem[i].fin != 1) {
+        i++;
+    }
+    return i;
 }
 
 void memory_destroy(memory mem) {
@@ -46,23 +58,73 @@ void memory_destroy(memory mem) {
 }
 
 int memory_read_byte(memory mem, uint32_t address, uint8_t *value) {
-    return -1;
+   if (address + 1 >= memory_get_size(mem)) {
+        return -1;
+    }
+    value = mem[address].value8;
+    return 0; 
 }
 
-int memory_read_half(memory mem, uint32_t address, uint16_t *value, uint8_t be) {
-    return -1;
+int memory_read_half(memory mem, uint32_t address, uint16_t *value, uint8_t be) { // gpt le frero 
+    // if (address + 1 >= memory_get_size(mem)) {
+    //     return -1;
+    // }
+    // if (be) {
+    //     // Big-endian byte order
+    //     *value = (uint16_t)(mem[address].value[0] << 8) | mem[address + 1].value[0];
+    // } else {
+    //     // Little-endian byte order
+    //     *value = (uint16_t)(mem[address + 1].value[0] << 8) | mem[address].value[0];
+    // }
+    return 0;
 }
 
-int memory_read_word(memory mem, uint32_t address, uint32_t *value, uint8_t be) {
-    return -1;
+int memory_read_word(memory mem, uint32_t address, uint32_t *value, uint8_t be) { //weeeeeeeeeeeesh // (gpt) ça charbonne ici wsh
+    if (mem == NULL || value == NULL) {
+        return -1;
+    }
+    if (address +3 >= memory_get_size(mem)) {
+        return -1; 
+    }
+    if (be) {
+        *value = (uint32_t)(mem[address].value[0] << 24) |
+                    (uint32_t)(mem[address + 1].value[0] << 16) |
+                        (uint32_t)(mem[address + 2].value[0] << 8) |
+                            (uint32_t)(mem[address + 3].value[0]);
+    } else {
+        *value = (uint32_t)(mem[address + 3].value[0] << 24) |
+                            (uint32_t)(mem[address + 2].value[0] << 16) |
+                            (uint32_t)(mem[address + 1].value[0] << 8) |
+                            (uint32_t)(mem[address].value[0]);
+    }
+    return 0;
+
 }
 
 int memory_write_byte(memory mem, uint32_t address, uint8_t value) {
-    return -1;
+    if (address + 1 >= memory_get_size(mem)) {
+        return -1;
+    }
+    mem[address].value8 = value; //cest pas bon// :thmubs down: //go essayer ça// c'est bon //non //si ça y est //non // normalement c'est bon // c'est toujours la cas ?
+    return 0;
 }
 
 int memory_write_half(memory mem, uint32_t address, uint16_t value, uint8_t be) {
-    return -1;
+    // if (address + 1 >= memory_get_size(mem)) {
+    //     return -1; // Out of bounds
+    // }
+
+    // if (be) {
+    //     // Big-endian byte order
+    //     mem[address].value = (value >> 8) & 0xFF;
+    //     mem[address + 1].value = value & 0xFF;
+    // } else {
+    //     // Little-endian byte order
+    //     mem[address + 1].value = (value >> 8) & 0xFF;
+    //     mem[address].value = value & 0xFF;
+    // }
+
+    return 0;
 }
 
 int memory_write_word(memory mem, uint32_t address, uint32_t value, uint8_t be) {
