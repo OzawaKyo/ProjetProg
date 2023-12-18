b35e3c799c7e003c6fccc05a7a204d7057b72378
Mohamed Amarcha <simoamarcha@icloud.com>
2023-12-18 16:56:04 +0100

    jour 3

--- arm_data_processing.c
+++ arm_data_processing.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_data_processing.h"
 #include "arm_exception.h"
@@ -26,13 +26,124 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_branch_other.h"
 #include "util.h"
 #include "debug.h"
+#include "registers.h"
+#include "arm_core.h"
 
 /* Decoding functions for different classes of instructions */
 int arm_data_processing_shift(arm_core p, uint32_t ins) {
-    return UNDEFINED_INSTRUCTION;
+	uint8_t opcode = (ins >> 21) & 0x0F;
+    switch (opcode) {
+		case 0x0:
+		return And(p, ins, 0);
+		case 0x1:
+		return Eor(p, ins);
+		case 0x2:
+		return Sub(p, ins);
+		case 0x3:
+		return Rsb(p, ins);
+		case 0x4:
+		return Add(p, ins);
+		case 0x5:
+		return Adc(p, ins);
+		case 0x6:
+		return Sbc(p, ins);
+		case 0x7:
+		return Rsc(p, ins);
+		case 0x8:
+		return Tst(p, ins);
+		case 0x9:
+		return Teq(p, ins);
+		case 0xA:
+		return Cmp(p, ins);
+		case 0xB:
+		return Cmn(p, ins);
+		case 0xC:
+		return Orr(p, ins);
+		case 0xD:
+		return Mov(p, ins);
+		case 0xE:
+		return Bic(p, ins);
+		case 0xF:
+		return Mvn(p, ins);
+		default:
+		return UNDEFINED_INSTRUCTION;
 }
-
+}
 int arm_data_processing_immediate_msr(arm_core p, uint32_t ins) {
     return UNDEFINED_INSTRUCTION;
 }
 
+/* Data processing instructions */	
+int And(arm_core p, uint32_t ins, uint8_t i) {
+	uint8_t s = (ins >> 20) & 0x01;
+	uint8_t rn = (ins >> 16) & 0x0F;
+	uint8_t rd = (ins >> 12) & 0x0F;
+	uint8_t shift = (ins >> 4) & 0x01;
+	uint8_t rm = ins & 0x0F;
+	uint32_t op2;
+	if (i==0)
+	{
+		if(shift == 0) {
+			op2 = registers_read(p->reg, rm, registers_get_mode(p->reg));
+		}
+		else {
+			
+		}
+	}
+	else
+	{
+		uint8_t rotate_imm = (ins >> 8) & 0x0F;
+		if (!rotate_imm)
+		{
+			op2 = ins & 0xFF;
+			// y'a un c flag dans shifter_operand
+		}
+		else{
+			//shifter_operand = (ins & 0xFF) << (rotate_imm*2);
+			op2 = (ins & 0xFF) >> (rotate_imm*2);
+			// y'a shifter_operand[31] dans shifter_operand
+		}
+		
+	}
+	
+
+	rd = rn + op2;
+}
+
+
+
+/*int Eor(arm_core p, uint32_t ins) {
+    uint8_t s = (ins >> 20) & 0x01;    
+    uint8_t rn = (ins >> 16) & 0x0F;   
+    uint8_t rd = (ins >> 12) & 0x0F;    
+    uint8_t i = (ins >> 25) & 0x01;     
+    uint8_t shift = (ins >> 4) & 0x01;  
+    uint8_t rm = ins & 0x0F;             
+
+    uint32_t op2;  
+
+    // Obtention de la valeur de op2 en fonction du mode d'adressage
+    if (!i && !shift) {
+        // Si pas de dÃ©calage immÃ©diat, op2 est la valeur du registre rm
+        op2 = p->reg[rm];
+
+    } else if (!i && shift) {
+        // Si dÃ©calage immÃ©diat, appliquer la fonction de dÃ©calage
+        uint8_t shift_type = (ins >> 5) & 0x03;     
+        uint8_t shift_amount = (ins >> 7) & 0x1F; 
+         if (shift_type == 0b00) {
+            // DÃ©calage logique Ã  gauche (LSL)
+            op2 = p->reg[rm] << shift_amount;
+        } else {
+            // Autres types de dÃ©calage non pris en charge
+            // GÃ©rer les autres cas si nÃ©cessaire
+            return UNDEFINED_INSTRUCTION;
+        }
+	} else {
+        op2 = ins & 0xFF;
+    }
+
+    p->reg[rd] = p->reg[rn] ^ op2;
+
+    return OK;
+}*/
--- arm_load_store.c
+++ arm_load_store.c
@@ -1,34 +1,210 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la 
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
+#include "arm_core.h"
 #include "arm_load_store.h"
 #include "arm_exception.h"
 #include "arm_constants.h"
 #include "util.h"
 #include "debug.h"
+#include "registers.h"
+
+#define ARM_INS_STORE 0
+#define ARM_INS_LOAD 1
+
+#define ARM_INS_LDR 0
+#define ARM_INS_LDRB 1
+//#define ARM_INS_LDRH 1
+//#define ARM_INS_LDM1 0
+
+#define ARM_INS_STR 0
+#define ARM_INS_STRB_STRH 1
+//#define ARM_INS_STRH
+
 
 int arm_load_store(arm_core p, uint32_t ins) {
-    return UNDEFINED_INSTRUCTION;
+    uint32_t ls = (ins >> 20); // Extract bit L from instruction
+    switch (ls)
+    {
+    case ARM_INS_STORE:
+        store(p, ins);
+        break;
+    case ARM_INS_LOAD:
+        load(p, ins);       
+        break;
+    default:
+        break;
+    }
 }
+
+int store(arm_core p, uint32_t ins){
+    uint32_t x = (ins >> 22); // Extract bit 22 from instruction
+    switch (x)
+    {
+    case ARM_INS_STR:
+        str(p, ins);
+        break;
+    
+    case ARM_INS_STRB_STRH:
+        strb_strh(p, ins);
+        break;
+        
+    default:
+        break;
+    }
+}
+
+
+int load(arm_core p, uint32_t ins){
+    uint32_t x = (ins >> 22);
+    switch (x) {
+        case 0:
+            ldr(p, ins);
+            break;
+        case 1:
+            ldrb_ldrh(p, ins);
+            break;
+        default:
+            break;
+    }
+}
+
+int str(arm_core p, uint32_t ins){
+    // bon mehdi je modifie tes fonctions un peu
+    uint8_t source = (ins >> 16) & 0x0F;
+	uint8_t dest = (ins >> 12) & 0x0F;
+    uint32_t cond = (ins >> 28); // necessaire ?
+
+    uint8_t be;
+    
+    mode_t mod = registers_get_mode(p->reg);
+    uint32_t value = registers_read(p->reg, source, mod);
+    memory_write_word(p->mem, dest, value, be);
+}
+
+int strb_strh(arm_core p, uint32_t ins){
+    uint32_t x = (ins >> 26); // Extract bit L from instruction
+    switch (x)
+    {
+    case 0:
+        strb(p, ins);
+        break;
+    case 1:
+        strh(p, ins);       
+        break;
+    default:
+        break;
+    }
+}
+
+int strb(arm_core p, uint32_t ins){
+    uint8_t source = (ins >> 16) & 0x0F;
+	uint8_t dest = (ins >> 12) & 0x0F;
+    uint32_t cond = (ins >> 28); // necessaire ?
+    
+    mode_t mod = registers_get_mode(p->reg);
+    uint32_t value = registers_read(p->reg, source, mod);
+    memory_write_byte(p->mem, dest, value);    
+}
+
+int strh(arm_core p, uint32_t ins){
+    uint8_t source = (ins >> 16) & 0x0F;
+	uint8_t dest = (ins >> 12) & 0x0F;
+    uint32_t cond = (ins >> 28); // necessaire ?
+
+    uint8_t be;
+    
+    mode_t mod = registers_get_mode(p->reg);
+    uint32_t value = registers_read(p->reg, source, mod);
+    memory_write_half(p->mem, dest, value, be);
+}
+
+
+
+
+
+int ldrb_ldrh(arm_core p, uint32_t ins) {
+    uint32_t x = (ins >> 26);
+    switch (x) {
+        case 1:
+            ldrb(p, ins);
+            break;
+        case 0:
+            ldrh(p, ins);
+            break;
+        default:
+            break;
+    }
+}
+
+
+int ldr(arm_core p, uint32_t ins) {
+    
+    uint8_t source = (ins >> 16) & 0x0F;
+    uint8_t dest = (ins >> 12) & 0x0F;
+    //uint32_t cond = (ins >> 28);
+
+    uint8_t mode = registers_get_mode(p->reg);
+    uint8_t value;
+    uint8_t be;
+
+    memory_read_word(p->mem, source, &value, be);
+    registers_write(p->reg, dest, mode, value); 
+
+}
+
+
+
+int ldrb(arm_core p, uint32_t ins) {
+    
+    uint8_t source = (ins >> 16) & 0x0F;
+    uint8_t dest = (ins >> 12) & 0x0F;
+    //uint32_t cond = (ins >> 28);
+
+    uint8_t mode = registers_get_mode(p->reg);
+    uint8_t value;
+
+    memory_read_byte(p->mem, source, &value);
+    registers_write(p->reg, dest, mode, value); 
+
+}
+
+
+int ldrh(arm_core p, uint32_t ins) {
+    
+    uint8_t source = (ins >> 16) & 0x0F;
+    uint8_t dest = (ins >> 12) & 0x0F;
+    //uint32_t cond = (ins >> 28);
+
+    uint8_t mode = registers_get_mode(p->reg);
+    uint8_t value;
+    uint8_t be;
+    
+    memory_read_half(p->mem, source, &value, be);
+    registers_write(p->reg, dest, mode, value); 
+
+}
+
+
 
 int arm_load_store_multiple(arm_core p, uint32_t ins) {
     return UNDEFINED_INSTRUCTION;
